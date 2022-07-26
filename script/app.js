bc0798d6bd5e1f0386de87f76949ea1d6eb14735
Elimelech23 <elimelechattale23@gmail.com>
2022-07-26 19:17:58 -0400

    Update app.js
    Modo claro y oscuro

--- script/app.js
+++ script/app.js
@@ -1,9 +1,19 @@
-function claro(){
-    if (document.getElementById("body").classList == ("bg-black")){
-        document.getElementById("body").classList.remove ("bg-black");
-        document.getElementById("body").classList.add ("bg-white");
-    } else if (document.getElementById("body").classList == ("bg-white")){
-        document.getElementById("body").classList.remove ("bg-white");
-        document.getElementById("body").classList.add ("bg-black");
-    }
+function OscuroClaro(){
+    document.body.classList.toggle("bg-gray-300")
+    document.getElementById("nav").classList.toggle("bg-gray-300/90")
+    document.getElementById("nav").classList.toggle("text-black")
+
+    document.getElementById("d-inicio").classList.toggle("bg-gray-300")
+    document.getElementById("titulop").classList.toggle("text-black")
+
+    document.getElementById("d-habilidades").classList.toggle("bg-gray-200")
+    document.getElementById("d-habilidades").classList.toggle("text-black")
+    document.getElementById("b-progress").classList.toggle("progress2")
+    document.getElementById("b-progress2").classList.toggle("progress2")
+    document.getElementById("b-progress3").classList.toggle("progress2")
+    document.getElementById("b-progress4").classList.toggle("progress2")
+    document.getElementById("b-progress5").classList.toggle("progress2")
+
+    document.getElementById("d-proyectos").classList.toggle("bg-gray-300")
+    document.getElementById("d-proyectos").classList.toggle("text-black")
 }
